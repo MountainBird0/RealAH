db1f564ebb9e7b2e4746bdc193fdcde701520b39
Kim Jae Hwi <99602963+MountainBird0@users.noreply.github.com>
2022-11-10 17:32:07 +0900

    [11.10] add ui
    .

--- Assets/00.a/Scripts/CheckWristMenu.cs
+++ Assets/00.a/Scripts/CheckWristMenu.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class CheckWristMenu : MonoBehaviour
 {
+    public GameObject button;
+    public GameObject wall;
+
     public void LookWrist()
     {
-        Debug.Log("¼Õ¸ñ º½");
+        button.gameObject.layer = 0;
+        button.SetActive(true);
     }
 
     public void LookWristOut()
     {
-        Debug.Log("¼Õ¸ñ ¾È º½");
+        button.SetActive(false);
+    }
+
+    public void TouchBu()
+    {
+        Debug.Log("ÅÍÄ¡ÀÌº¥Æ®µé°¨");
+
+        if (wall.activeSelf)
+        {
+            Debug.Log("º® ¼û±è");
+
+            wall.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("º® ³ª¿È");
+
+            wall.SetActive(true);
+        }
+
     }
 }
--- Assets/00.a/Scripts/MovePlayerPrac.cs
+++ Assets/00.a/Scripts/MovePlayerPrac.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 
 public class MovePlayerPrac : MonoBehaviour
 {
-    public GameObject player;
-
     public List<GameObject> point;
 
-    AutoHandPlayer playerBody;
+    public AutoHandPlayer playerBody;
+
 
     private void Start()
     {
-        playerBody = FindObjectOfType<AutoHandPlayer>();
+        // playerBody = FindObjectOfType<AutoHandPlayer>();
+        Debug.Log(playerBody.name);
     }
 
 
@@ -21,13 +21,13 @@ public class MovePlayerPrac : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            //player.transform.position = point[0].transform.position;
-            playerBody.SetPosition(point[0].transform.position);
+            playerBody.transform.position = point[0].transform.position;
+            //playerBody.SetPosition(point[0].transform.position);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            //player.transform.position = point[1].transform.position;
+            //playerBody.transform.position = point[1].transform.position;
             playerBody.SetPosition(point[1].transform.position);
 
         }
