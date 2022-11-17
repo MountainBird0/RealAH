96e67accf4b9ab97129ca047218ad79e05851905
Kim Jae Hwi <99602963+MountainBird0@users.noreply.github.com>
2022-11-17 17:39:28 +0900

    [11.17] for driver
    .

--- Assets/00.a/Scripts/dot/dotmove.cs
+++ Assets/00.a/Scripts/dot/dotmove.cs
@@ -5,23 +5,20 @@ using DG.Tweening;
 
 public class dotmove : MonoBehaviour
 {
-    
-
+    public GameObject cube;
 
     void Start()
     {
-        Tweener move1;
-        Tweener move2;
-
         Sequence seq = DOTween.Sequence();
-        move1 = transform.DOMove(Vector3.one, 3f);
-        move2 = transform.DOMove(Vector3.zero, 3f);
-
-        seq.Append(move1);
-        seq.Append(move2);
         
-        //seq.Append(obj.transform.DOScale(Vector3.zero, 3f)).
-        //    Prepend(obj.transform.DOMove(Vector3.one, 3f));
+        Tweener move1 = transform.DOMove(Vector3.one, 3f);
+        Tweener move2 = transform.DOMove(Vector3.zero, 3f);
 
+        //move1 = cube.transform.DOMove(Vector3.one, 3f);
+        //move2 = cube.transform.DOMove(Vector3.zero, 3f);
+
+        //seq.Append(move1).Append(move2);
+
+        cube.transform.DOMoveX(200, 2f).SetRelative();
     }
 }
